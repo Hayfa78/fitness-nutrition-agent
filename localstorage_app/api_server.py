12db2ac656830cd2e7fc1c3b301a6568e6d1cacc
Hayfa78 <hayfa8siblini@gmail.com>
2026-04-26 23:46:13 +0300

    update api url to render

--- localstorage_app/api_server.py
+++ localstorage_app/api_server.py
@@ -74,7 +74,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:8504",
-        "http://127.0.0.1:8504",
+        "https://fitness-nutrition-agent.onrender.com",
         "http://localhost:3000",
     ],
     allow_credentials=True,
